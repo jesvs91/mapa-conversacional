bfb18a870e6d3792596987175113c02336135807
jesvs91 <jesvs91@gmail.com>
2024-12-29 00:33:25 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
+document.querySelectorAll('#mapa-index a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+       const targetId = this.getAttribute('href').substring(1);
+          document.querySelectorAll('#mapa-index a').forEach(a => {
+             a.classList.remove('active');
+        });
+        this.classList.add('active');
+        document.querySelectorAll('.detalle-container').forEach(detalle => {
+            detalle.style.display = 'none';
+        });
+           document.querySelectorAll('.nodo-principal').forEach(nodo => {
+            nodo.classList.remove('active');
+        });
+        const detailContainer = document.getElementById(`${targetId}-detalle`);
+         const nodoPrincipal= document.getElementById(targetId);
+        if(detailContainer)detailContainer.style.display = 'block';
+          if(nodoPrincipal) nodoPrincipal.classList.add('active');
+     const mapaFlujo = document.getElementById('mapa-flujo');
+     if(mapaFlujo) mapaFlujo.scrollLeft=nodoPrincipal.offsetLeft - 100;
+    });
+});
+
 document.querySelectorAll('.nodo-principal').forEach(nodo => {
-    nodo.addEventListener('click', function(e) {
-       if(e.target.classList.contains('opcion')) return;
+   nodo.addEventListener('click', function(e) {
+       if(e.target.classList.contains('opcion') || e.target.nodeName=="A") return;
         const target = this.getAttribute('data-target');
         const detailContainer = document.getElementById(target);
         document.querySelectorAll('.detalle-container').forEach(detalle => {
@@ -14,30 +37,15 @@ document.querySelectorAll('.nodo-principal').forEach(nodo => {
     });
 });
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        const targetId = this.getAttribute('href').substring(1);
-
-          document.querySelectorAll('.nodo-principal').forEach(nodo => {
-            nodo.classList.remove('active');
-        });
-         document.getElementById(targetId).classList.add('active');
-      document.querySelectorAll('.detalle-container').forEach(detalle => {
-            detalle.style.display = 'none';
-        });
-         const detailContainer = document.getElementById(`${targetId}-detalle`);
-          if(detailContainer)detailContainer.style.display = 'block';
-
-    });
-});
-
-
- function regresar() {
+  function regresar() {
         document.querySelectorAll('.detalle-container').forEach(detalle => {
              detalle.style.display = 'none';
         });
-         document.querySelectorAll('.nodo-principal').forEach(nodo => {
+          document.querySelectorAll('.nodo-principal').forEach(nodo => {
             nodo.classList.remove('active');
         });
+          document.querySelectorAll('#mapa-index a').forEach(a => {
+             a.classList.remove('active');
+        });
+
     }
